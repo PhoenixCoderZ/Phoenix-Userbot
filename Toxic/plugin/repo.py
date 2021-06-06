2cbeede49806e4a223272f6adc6522121c7c45ff
itznik <75500096+itznik@users.noreply.github.com>
2021-06-06 15:51:35 +0530

    Update repo.py

--- Toxic/plugin/repo.py
+++ Toxic/plugin/repo.py
@@ -8,8 +8,9 @@ cmd = cmd(filters.command, filters.me, prefix=["."])
 @Client.on_message(cmd("repo"))
 async def repo(client: Client, message: Message):
     await message.edit(f'''<b>---THE TOXIC USERBOT---
-• UserboT Is AlIVe
+• ToXic Is AlIVe
 • License: GNU GPL v3.0
 • <a href="https://t.me/ToxicUb_Support">SUPPORT</a>
 • <a href="https://github.com/Khush-Botz/Toxic-Userbot">GITHUB</a>
 </b>''')
+• ToXic UB: Made With Lub❤️
