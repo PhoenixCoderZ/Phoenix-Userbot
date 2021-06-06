3a2be0f194ae57ade37fea2c72c61e2fb048c8cd
Hacker Hero <67130996+pr13260@users.noreply.github.com>
2021-06-06 13:38:48 +0530

    Adde first plug 🤣🤣

---
+++ Toxic/plugin/repo.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+@Client.on_message(filters.command('repo', ['.']) & filters.me)
+async def repo(client: Client, message: Message):
+    await message.edit(f'''<b>---THE TOXIC USERBOT---
+• UserboT Is AlIVe
+• License: GNU GPL v3.0
+• <a href="https://t.me/ToxicUb_Support">SUPPORT</a></b>''')
+
+
+utils.modules_help.update({'repo': '''repo - Userbot information''', 'repo module': 'Repo: repo'})
