afc603b5e1421a7a6db975e8f4af4ae44e371b37
Hacker Hero <67130996+pr13260@users.noreply.github.com>
2021-06-06 20:35:09 +0530

    Sed vro added ping plug

---
+++ Toxic/plugin/ping.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from pyrogram.types import Message
+from pyrogram import filters
+
+
+@Client.on_message(filters.command("ping", ".") & filters.me)
+async def pingme(_, message: Message):
+    start = datetime.now()
+    await message.edit("`Pong!`")
+    end = datetime.now()
+    m_s = (end - start).microseconds / 1000
+    await message.edit(f"**Pong!**\n`{m_s} ms`")
