12d843dc132d528bf1bf19c79a1b6633282242ca
Hacker Hero <67130996+pr13260@users.noreply.github.com>
2021-06-06 21:05:26 +0530

    3 moreee plugg

---
+++ Toxic/plugin/google.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@Client.on_message(filters.command(['google', 'g'], ["."]) & filters.me)
+async def webshot(client: Client, message: Message):
+    user_request = ' '.join(message.command[1:])
+
+    if user_request == '':
+        if message.reply_to_message:
+            reply_user_request = message.reply_to_message.text
+            request = reply_user_request.replace(' ', '+')
+            full_request = f'https://lmgtfy.app/?s=g&iie=1&q={request}'
+            await message.edit(f'<a href={full_request}>{reply_user_request}</a>')
+
+    else:
+        request = user_request.replace(' ', '+')
+        full_request = f'https://lmgtfy.app/?s=g&iie=1&q={request}'
+        await message.edit(f'<a href={full_request}>{user_request}</a>')
+
+
+modules_help.update({'google': '''google [request] - To teach the interlocutor to use Google, 
+                                  google - Answer a stupid question of your interlocutor to teach him how to use Google''',
+                     'google module': 'Google: google'})
