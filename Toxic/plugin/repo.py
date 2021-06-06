1be2e4e2176b3b97e01070f8a15d7754eda286b8
Hacker Hero <67130996+pr13260@users.noreply.github.com>
2021-06-06 13:42:13 +0530

    Fuk forgot git
    #shaste_nashe

--- Toxic/plugin/repo.py
+++ Toxic/plugin/repo.py
@@ -7,7 +7,9 @@ async def repo(client: Client, message: Message):
     await message.edit(f'''<b>---THE TOXIC USERBOT---
 • UserboT Is AlIVe
 • License: GNU GPL v3.0
-• <a href="https://t.me/ToxicUb_Support">SUPPORT</a></b>''')
+• <a href="https://t.me/ToxicUb_Support">SUPPORT</a>
+• <a href="https://github.com/Khush-Botz/Toxic-Userbot">GITHUB</a>
+</b>''')
 
 
 utils.modules_help.update({'repo': '''repo - Userbot information''', 'repo module': 'Repo: repo'})
