6ad12fd1bc17ec910c0018feabc58d738d621a8c
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-06-11 21:16:04 +0530

    Update sample_config.py

--- Toxic/sample_config.py
+++ Toxic/sample_config.py
@@ -7,6 +7,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     MAX_MESSAGE_LENGTH = 4096
+    TG_MAX_SELECT_LEN = 4096
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", ".")
     TMP_DOWNLOAD_DIRECTORY = os.environ.get(
         "TMP_DOWNLOAD_DIRECTORY", "/root/toxic/downloads/"
