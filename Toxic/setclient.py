0d19428917c33c6683c43c082c47ce8b50f9e279
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-06-11 22:01:14 +0530

    Update setclient.py

--- Toxic/setclient.py
+++ Toxic/setclient.py
@@ -1,8 +1,10 @@
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from Toxic.plugins import ALL_PLUGINS
-from Toxic import APP_ID, API_HASH, STRING_SESSION, LOGGER, load_cmds
+from Toxic import APP_ID, API_HASH, STRING_SESSION, USERBOT_PREFIX, LOGGER, load_cmds
+from functools import partial as cmd
 
+cmd = cmd(filters.command, filters.me, prefix=USERBOT_PREFIX)
 
 class Toxic(Client):
     def __init__(self):
