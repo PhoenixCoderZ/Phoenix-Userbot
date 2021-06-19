47feb425a4e8dd756e332b1e206068df3fa6ebe6
itznik <75500096+itznik@users.noreply.github.com>
2021-06-19 19:39:35 +0530

    Add files via upload

---
+++ Phoenix/versions.py
@@ -0,0 +1,11 @@
+from sys import version_info
+
+from pyrogram import __version__ as __pyro_version__
+
+__major__ = 0
+__minor__ = 5
+__micro__ = 0
+
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+__license__ = "[GNU GPL v3.0](https://github.com/PhoenixCoderZ/Phoenix-Userbot/blob/master/LICENSE)"
+__copyright__ = "[PhoenixTeam](https://github.com/PhoenixCoderZ/Phoenix-Userbot/)"
