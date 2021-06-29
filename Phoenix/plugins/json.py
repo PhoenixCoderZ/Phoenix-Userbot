3f1aa5f3f8151d0183f801f25ca1f337d302845e
Khushmeet-op <78538877+Khushmeet-op@users.noreply.github.com>
2021-06-29 09:47:55 +0530

    Create json.py

---
+++ Phoenix/plugins/json.py
@@ -0,0 +1,34 @@
+from pyrogram.types import *
+from Phoenix import *
+from pyrogram import *
+from config import *
+from os import *
+
+@Phoenix.on_message(filters.command("json", PREFIX) & filters.me)
+async def start(client, message):
+    try:
+        if message.reply_to_message:
+            msg = message.reply_to_message
+
+        else:
+            msg = message
+
+        msg_info = str(msg)
+
+        if len(msg_info) > int("4096"):
+            file = open("json.txt", "w+")
+            file.write(msg_info)
+            file.close()
+            await app.send_document(
+                message.chat.id,
+                "json.txt",
+                caption="Returned JSon",
+
+            )
+            remove("json.txt")
+
+        else:
+            await message.edit(msg_info)
+
+    except Exception as e:
+        await message.edit(f"{e}")
